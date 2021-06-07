37fbce879b5fdc23620dbe80b19926bd1f6a218f
Administrator <18292099806>
2021-06-07 10:42:43 +0800

    add code

--- jvm/src/main/java/JiangzhiOffer/FindInPartiallySortedMatrix/Solution.java
+++ jvm/src/main/java/JiangzhiOffer/FindInPartiallySortedMatrix/Solution.java
@@ -3,18 +3,27 @@ package JiangzhiOffer.FindInPartiallySortedMatrix;
 public class Solution {
     private static final int[][] INTS = new int[][]{{1,2,3},{2,3,4},{3,4,5}};
 
+    /**
+     * 从右上角开始查找
+     * 1，若target小于该元素，则k--
+     * 2，若target大于该元素，则j++
+     * 3，如target等于该元素，则返回true
+     * @param ints
+     * @param target
+     * @return
+     */
     private static boolean solution(int[][] ints,int target){
         if (ints == null){
             return false;
         }
         int le = ints.length;
         int ro = ints[0].length;
-        int j=le;
-        int k = ro;
-        while (j >= 0 && k >=0 && le >= j-1 && ro >= k-1 ){
-            if (ints[j][k] > target){
+        int j = 0;
+        int k = ro -1;
+        while (0<=j && j<= le-1 && 0<=k && k <= ro-1){
+            if (target < ints[j][k]){
                 k--;
-            }else if (ints[j][k] < target){
+            }else if (target > ints[j][k]){
                 j++;
             }else {
                 return true;
@@ -22,7 +31,37 @@ public class Solution {
         }
         return false;
     }
+
+    /**
+     * 从左下角开始查找
+     * 1、若target小于该元素，则j--
+     * 2、若target大于该元素，则k++
+     * 3、若相等，则返回true
+     * @param ints
+     * @param target
+     * @return
+     */
+    private static boolean solution2(int[][] ints,int target){
+        if (ints == null){
+            return false;
+        }
+        int le = ints.length;
+        int ro = ints[0].length;
+        int j = le-1;
+        int k = 0;
+        while (0<=j&&j<=le-1 && 0<=k && k <= ro-1){
+            if (target < ints[j][k]){
+                j--;
+            }else if (target > ints[j][k]){
+                k++;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        System.out.println(solution(INTS,9));
+        System.out.println(solution(INTS,3));
+        System.out.println(solution2(INTS,3));
     }
 }
