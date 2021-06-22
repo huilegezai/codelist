bc48beef3f0d20bf490faed64c454467633e634b
Administrator <18292099806>
2021-06-22 11:02:02 +0800

    回文字符

--- jvm/src/main/java/leetcode/Solution.java
+++ jvm/src/main/java/leetcode/Solution.java
@@ -613,8 +613,28 @@ public class Solution {
      * @param s
      * @return
      */
-    public boolean isPalindrome(String s) {
-
+    public static boolean isPalindrome(String s) {
+        //这题没什么难度，最简单的就是使用双指针，一个指向前，一个指向后，遇到空格以及特殊字符要跳过，然后判断，画个图来看下
+        if (s.length() ==0){
+            return true;
+        }
+        int left =0,right = s.length()-1;
+        while (left<right){
+            //因为题中说了，只考虑字母和数字，所以不是字母和数字的先过滤掉
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            //然后把两个字符变为小写，在判断是否一样，如果不一样，直接返回false
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -637,5 +657,6 @@ public class Solution {
         System.out.println(lengthOfLastWord("iiir"));
         System.out.println(singleNumber(new int[]{-2, -2, 1, -3, -1, -3, 1, 4, 4}));
         System.out.println(majorityElement(new int[]{-2, -2, -2, -2, -2, -2, 1, 4, 4}));
+        System.out.println(isPalindrome("qwwq"));
     }
 }
